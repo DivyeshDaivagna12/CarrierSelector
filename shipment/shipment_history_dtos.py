bf98b218411ff0bebfc2c8c8749cfbd0f0a24041
Divyesh Daivagna <Divyesh.Daivagna@purolator.com>
2024-03-11 18:36:59 +0530

    ManishChanges

---
+++ shipment/shipment_history_dtos.py
@@ -0,0 +1,19 @@
+
+from ast import List
+from typing import List, Optional
+
+
+class ShipmentHistoryDetailDto:
+        customer:str
+        origin_address:str
+        shipment_id:str
+        carrier_id: str
+        shipment_date: int
+ 
+class ShipmentHistorySetDto:
+        customer:str
+        origin_address:str
+        shipment_id:str
+        carrier_id: str
+        shipment_date: int
+
